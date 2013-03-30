e342bb5c12c150f240050aaf7c3bf77f68a8ea48
yang-chao <y.yangchao@gmail.com>
2013-03-31 00:38:36 +0800

    add recipe activity

---
+++ GoCook/src/com/m6/gocook/biz/recipe/RecipeActivity.java
@@ -0,0 +1,63 @@
+package com.m6.gocook.biz.recipe;
+
+import com.m6.gocook.R;
+import android.app.Activity;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.GridView;
+import android.widget.TextView;
+
+public class RecipeActivity extends Activity {
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_recipe);
+		
+		setTitle("葱油饼");
+		
+		initView();
+	}
+	
+	private void setTitle(String title) {
+		((TextView) findViewById(R.id.actionbar_title)).setText(title);
+	}
+	
+	private void initView() {
+
+		((TextView)this.findViewById(R.id.tabbar_textview_buy)).setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				((TextView)v).setCompoundDrawablesWithIntrinsicBounds(null,
+						getResources().getDrawable(R.drawable.recipe_tabbar_bought),
+						null, null);
+			}
+		});
+		
+
+		((TextView)this.findViewById(R.id.tabbar_textview_like)).setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				((TextView)v).setCompoundDrawablesWithIntrinsicBounds(null,
+						getResources().getDrawable(R.drawable.recipe_tabbar_likehl),
+						null, null);
+			}
+		});
+		
+
+		((TextView)this.findViewById(R.id.tabbar_textview_upload)).setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				
+			}
+		});
+	}
+	
+}
