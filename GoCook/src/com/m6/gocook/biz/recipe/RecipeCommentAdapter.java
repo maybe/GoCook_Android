00dae39a045a6b81cefe76bc9b8bc128daf8107a
yang-chao <y.yangchao@gmail.com>
2013-04-07 17:27:06 +0800

    add recipe comment activity

---
+++ GoCook/src/com/m6/gocook/biz/recipe/RecipeCommentAdapter.java
@@ -0,0 +1,59 @@
+package com.m6.gocook.biz.recipe;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+import com.m6.gocook.R;
+
+public class RecipeCommentAdapter extends BaseAdapter {
+
+	private LayoutInflater mInflater;
+	
+	public RecipeCommentAdapter(Context context) {
+		mInflater = LayoutInflater.from(context);
+	}
+	
+	@Override
+	public int getCount() {
+		return 11;
+	}
+
+	@Override
+	public Object getItem(int position) {
+		return null;
+	}
+
+	@Override
+	public long getItemId(int position) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public View getView(int position, View convertView, ViewGroup parent) {
+		ViewHold holder;
+		if(convertView == null) {
+			convertView = mInflater.inflate(R.layout.adapter_recipe_comment_item, null);
+			holder = new ViewHold();
+			holder.content = (TextView) convertView.findViewById(R.id.content);
+			holder.date = (TextView) convertView.findViewById(R.id.date);
+			convertView.setTag(holder);
+		} else {
+			holder = (ViewHold) convertView.getTag();
+		}
+		
+		holder.content.setText("做了效果很好！很好吃很好吃很好吃很好吃很好吃很好吃很好吃很好吃很好吃很好吃很好吃很好吃很好吃很好吃很好吃很好吃");
+		holder.date.setText("2013-04-03 14：34：65");
+		return convertView;
+	}
+	
+	private class ViewHold {
+		private TextView content;
+		private TextView date;
+	}
+
+}
---
+++ GoCook/src/com/m6/gocook/biz/recipe/RecipeCommentsActivity.java
@@ -0,0 +1,29 @@
+package com.m6.gocook.biz.recipe;
+
+import com.m6.gocook.R;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.ListView;
+import android.widget.TextView;
+
+public class RecipeCommentsActivity extends Activity {
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+
+		setContentView(R.layout.activity_recipe_comments);
+		
+		setTitle("葱油饼");
+		
+		ListView list = (ListView) findViewById(R.id.comments_listview);
+		list.setAdapter(new RecipeCommentAdapter(this));
+		
+		super.onCreate(savedInstanceState);
+	}
+	
+	private void setTitle(String title) {
+		((TextView) findViewById(R.id.actionbar_title)).setText(title);
+	}
+
+}
