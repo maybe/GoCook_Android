c94573671d2cff79fb8bcdf46258ec14995c7d27
杨超 <yangchao294@126.com>
2013-07-14 16:28:50 +0800

    add my recipes

---
+++ GoCook/src/com/m6/gocook/biz/recipe/my/MyRecipesFragment.java
@@ -0,0 +1,51 @@
+package com.m6.gocook.biz.recipe.my;
+
+import android.os.Bundle;
+
+import com.m6.gocook.R;
+import com.m6.gocook.base.entity.RecipeList;
+import com.m6.gocook.base.view.ActionBar;
+import com.m6.gocook.biz.account.AccountModel;
+import com.m6.gocook.biz.recipe.RecipeModel;
+import com.m6.gocook.biz.recipe.list.RecipeListFragment;
+
+public class MyRecipesFragment extends RecipeListFragment {
+
+	public static final String PARAM_FROM_PROFILE = "param_from_profile";
+	
+	private boolean mFromProfile = false; // 从我的个人资料页面跳转而来就从本地取数据，否则取网络数据
+	
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		Bundle args = getArguments();
+		if(args != null) {
+			mFromProfile = args.getBoolean(PARAM_FROM_PROFILE);
+		}
+	}
+	
+	@Override
+	public void onActivityCreated(Bundle savedInstanceState) {
+		super.onActivityCreated(savedInstanceState);
+
+		ActionBar action = getActionBar();
+		String username = AccountModel.getUsername(getActivity());
+		action.setTitle(getString(R.string.biz_profile_mycollection_title, username));
+	}
+
+	@Override
+	protected String getURL() {
+		return null; // 数据从getListData中得到，不需要url
+	}
+	
+	@Override
+	protected boolean doPaging() {
+		return false;
+	}
+	
+	@Override
+	protected RecipeList getListData(String url) {
+		return RecipeModel.getMyRecipes(getActivity(), mFromProfile);
+	}
+	
+}
