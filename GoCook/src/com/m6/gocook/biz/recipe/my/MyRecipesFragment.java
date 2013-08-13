b71c414761298bb6229f7a40bcb775c73fb788d3
杨超 <yangchao294@126.com>
2013-08-13 22:23:37 +0800

    fix myrecipes bug

--- GoCook/src/com/m6/gocook/biz/recipe/my/MyRecipesFragment.java
+++ GoCook/src/com/m6/gocook/biz/recipe/my/MyRecipesFragment.java
@@ -31,20 +31,16 @@ public class MyRecipesFragment extends RecipeListFragment {
 	
 	@Override
 	public View onCreateHeaderView(LayoutInflater inflater, ViewGroup container) {
-		if (mFromPersonnalProfile) {
-			View view = inflater.inflate(R.layout.fragment_myrecipes_header, container, false);
-			view.setOnClickListener(new OnClickListener() {
-				
-				@Override
-				public void onClick(View v) {
-					RecipeEditFragment.startInActivity(getActivity(),
-							RecipeEditFragment.Mode.RECIPE_NEW, "");
-				}
-			});
-			return view;
-		} else {
-			return null;
-		}
+		View view = inflater.inflate(R.layout.fragment_myrecipes_header, container, false);
+		view.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				RecipeEditFragment.startInActivity(getActivity(),
+						RecipeEditFragment.Mode.RECIPE_NEW, "");
+			}
+		});
+		return view;
 	}
 	
 	@Override
